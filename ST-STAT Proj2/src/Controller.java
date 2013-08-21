50627a0197bdf13d9c3f31436e16fa6b5667af91
tintin.kalaw@gmail.com <tintin.kalaw@gmail.com@c94183e0-69c1-b9a1-e06d-8ebbc50ae90b>
2013-08-21 13:35:54 +0000

    - error checking for inputs for all tabs - can compute for hypergeom given a set of inputs - can compute for hypergeom given a set of inputs, range of x - heavily commented - improved MVC

--- ST-STAT Proj2/src/Controller.java
+++ ST-STAT Proj2/src/Controller.java
@@ -1,26 +1,106 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-
+/**
+ * CONTROLLER:
+ * This links the VIEW and the MODEL
+ */
 public class Controller {
 	private View myView;
 	private Model myModel;
 	
+	/**
+	 * Controller Constructor
+	 * @param view - GUI
+	 * @param model - Functionalities
+	 */
 	public Controller(View view, Model model) {
 		this.myView = view;
 		this.myModel = model;
 		
-		this.myView.addTabSelectionListener(new tabSelectionListener());
+		this.myView.addMenuItemClearFieldsListener(new MenuItemClrFldsListener());
+		this.myView.addMenuItemSpecificationsListener(new MenuItemSpecsListener());
+		this.myView.addMenuItemAboutListener(new MenuItemAboutListener());
+		this.myView.addTabSelectionListener(new TabSelectListener());
+	}
+	
+	/*
+	 * MENU ITEM LISTENERS
+	 */
+	
+	/**
+	 * Listener for CLEAR FIELDS menu item.
+	 * Clears the fields
+	 */
+	class MenuItemClrFldsListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			myView.getTxtBigN().setText(null);
+			myView.getTxtBigN().setBackground(Color.WHITE);
+			
+			myView.getTxtSmallN1().setText(null);
+			myView.getTxtSmallN1().setBackground(Color.WHITE);
+			
+			myView.getTxtSmallN2().setText(null);
+			myView.getTxtSmallN2().setBackground(Color.WHITE);
+			
+			myView.getTxtK1().setText(null);
+			myView.getTxtK1().setBackground(Color.WHITE);
+			
+        	myView.getTxtK2().setText(null);
+        	myView.getTxtK2().setBackground(Color.WHITE);
+        	
+        	myView.getTxtX1().setText(null);
+        	myView.getTxtX1().setBackground(Color.WHITE);
+        	
+        	myView.getTxtX2().setText(null);
+        	myView.getTxtX2().setBackground(Color.WHITE);
+        	
+        	System.out.println("Clear Fields");
+		}
 	}
 	
 	/**
+	 * Listener for SPECIFICATIONS menu item.
+	 * Displays the project specifications
+	 */
+	class MenuItemSpecsListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+
+			System.out.println("View Specs");
+		}
+	}
+	
+	/**
+	 * Listener for ABOUT menu item.
+	 * Displays what this project is about
+	 */
+	class MenuItemAboutListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			System.out.println("About");
+		}
+	}
+	
+	/*
 	 * TAB SELECTION LISTENER
 	 */
-	class tabSelectionListener implements ChangeListener {
+	
+	/**
+	 * Listener for TAB SELECTION
+	 */
+	class TabSelectListener implements ChangeListener {
 
 		@Override
 		public void stateChanged(ChangeEvent e) {
@@ -29,47 +109,268 @@ public class Controller {
 	        
 	        System.out.println("selectedIndex = " + index);
 	        
-	        if(index == 0) {
+	        switch(index) {
+	        case 0: // FIXED PARAMETERS
 	        	myView.getTab1().add(myView.getInputPanel());
 	        	myView.getTab1().add(myView.getContentPanel());
 	        	myView.getTab1().add(myView.getAnswerPanel());
 	        	myView.getTab1().add(myView.getProbDistPanel());
-	        	myView.addBtnSolveListener(new solveTab1Listener());
-	        } else if(index == 1) {
+	        	
+	        	myView.getTxtSmallN2().setEditable(false);
+	        	myView.getTxtK2().setEditable(false);
+	        	myView.getTxtX2().setEditable(false);
+	        	
+	        	myView.addBtnSolveListener(new SolveTab1Listener());
+	        	break;
+	        case 1: // RANGE OF 'x'
 	        	myView.getTab2().add(myView.getInputPanel());
 	        	myView.getTab2().add(myView.getContentPanel());
 	        	myView.getTab2().add(myView.getAnswerPanel());
 	        	myView.getTab2().add(myView.getProbDistPanel());
-	        } else if(index == 2) {
+	        	
+	        	myView.getTxtSmallN2().setEditable(false);
+	        	myView.getTxtK2().setEditable(false);
+	        	myView.getTxtX2().setEditable(true);
+	        	
+	        	myView.addBtnSolveListener(new SolveTab2Listener());
+	        	break;
+	        case 2: // 'n' INCREASES
 	        	myView.getTab3().add(myView.getInputPanel());
 	        	myView.getTab3().add(myView.getContentPanel());
 	        	myView.getTab3().add(myView.getAnswerPanel());
 	        	myView.getTab3().add(myView.getProbDistPanel());
-	        } else if(index == 3) {
+	        	
+	        	myView.getTxtSmallN2().setEditable(true);
+	        	myView.getTxtK2().setEditable(false);
+	        	myView.getTxtX2().setEditable(false);
+	        	
+	        	myView.addBtnSolveListener(new SolveTab3Listener());
+	        	break;
+	        case 3: // 'k' INCREASES
 	        	myView.getTab4().add(myView.getInputPanel());
 	        	myView.getTab4().add(myView.getContentPanel());
 	        	myView.getTab4().add(myView.getAnswerPanel());
 	        	myView.getTab4().add(myView.getProbDistPanel());
+	        	
+	        	myView.getTxtSmallN2().setEditable(false);
+	        	myView.getTxtK2().setEditable(true);
+	        	myView.getTxtX2().setEditable(false);
+	        	
+	        	myView.addBtnSolveListener(new SolveTab4Listener());
+	        	break;
 	        }
 		}
 	}
 	
-	class solveTab1Listener implements ActionListener {
+	/*
+	 * INPUT ERROR CHECKING
+	 */
+	
+	/**
+	 * Checks if inputs for N, n, k, and x are valid.
+	 * @return true if fields for N, n, k, and x are not empty and if x <= n and x <= k
+	 */
+	private boolean isMainInputValid() {
+		boolean valid = false;
+		
+		try {
+			myView.getTxtBigN().setBackground(Color.WHITE);
+			myModel.setBigN(myView.getBigN());
+			valid = true;
+		} catch (Exception ex) {
+			myView.getTxtBigN().setBackground(Color.PINK);
+			valid = false;
+		}
+		
+		try {
+			myView.getTxtSmallN1().setBackground(Color.WHITE);
+			myModel.setSmallN1(myView.getSmallN1());
+			valid = true;
+		} catch (Exception ex) {
+			myView.getTxtSmallN1().setBackground(Color.PINK);
+			valid = false;
+		}
+		
+		try {
+			myView.getTxtK1().setBackground(Color.WHITE);
+			myModel.setK1(myView.getK1());
+			valid = true;
+		} catch (Exception ex) {
+			myView.getTxtK1().setBackground(Color.PINK);
+			valid = false;
+		}
+		
+		try {
+			myView.getTxtX1().setBackground(Color.WHITE);
+			myModel.setX1(myView.getX1());
+			valid = true;
+		} catch (Exception ex) {
+			myView.getTxtX1().setBackground(Color.PINK);
+			valid = false;
+		}
+		
+		if (valid) {
+			if (myModel.getX1() <= myModel.getSmallN1() && myModel.getX1() <= myModel.getK1()) {
+				valid = true;
+			} else {
+				JOptionPane.showMessageDialog(myView, "x <= n ; x <= k", "Error!", JOptionPane.ERROR_MESSAGE);
+				valid = false;
+			}
+		}
+		
+		return valid;
+	}
+	
+	/**
+	 * Checks if input for the upper bound of n is valid
+	 * @return true if field for upper bound of n is not empty and lower bound n <= upper bound n
+	 */
+	private boolean isInputN2Valid() {
+		boolean valid = false;
+		
+		try {
+			myView.getTxtSmallN2().setBackground(Color.WHITE);
+			myModel.setSmallN2(myView.getSmallN2());
+			valid = true;
+		} catch (Exception ex) {
+			myView.getTxtSmallN2().setBackground(Color.PINK);
+			valid = false;
+		}
+		
+		if (valid) {
+			if(myModel.getSmallN1() <= myModel.getSmallN2()) {
+				valid = true;
+			} else {
+				JOptionPane.showMessageDialog(myView, "n1 <= n2", "Error!", JOptionPane.ERROR_MESSAGE);
+				valid = false;
+			}
+		}
+		
+		return valid;
+	}
+	
+	/**
+	 * Checks if input for the upper bound of k is valid
+	 * @return true if field for upper bound of k is not empty and lower bound k <= upper bound k
+	 */
+	private boolean isInputK2Valid() {
+		boolean valid = false;
+		
+		try {
+			myView.getTxtK2().setBackground(Color.WHITE);
+			myModel.setK2(myView.getK2());
+			valid = true;
+		} catch (Exception ex) {
+			myView.getTxtK2().setBackground(Color.PINK);
+			valid = false;
+		}
+		
+		if (valid) {		
+			if(myModel.getK1() <= myModel.getK2()) {
+				valid = true;
+			} else {
+				JOptionPane.showMessageDialog(myView, "k1 <= k2", "Error!", JOptionPane.ERROR_MESSAGE);
+				valid = false;
+			}
+		}
+		
+		return valid;
+	}
+
+	/**
+	 * Checks if input for the upper bound of x is valid
+	 * @return true if field for upper bound of x is not empty and lower bound x <= upper bound x
+	 */
+	private boolean isInputX2Valid() {
+		boolean valid = false;
+		
+		try {
+			myView.getTxtX2().setBackground(Color.WHITE);
+			myModel.setX2(myView.getX2());
+			valid = true;
+		} catch (Exception ex) {
+			myView.getTxtX2().setBackground(Color.PINK);
+			valid = false;
+		}
+		
+		if (valid) {
+			if(myModel.getX1() <= myModel.getX2()) {
+				valid = true;
+			} else {
+				JOptionPane.showMessageDialog(myView, "x1 <= x2", "Error!", JOptionPane.ERROR_MESSAGE);
+				valid = false;
+			}
+		}
+		
+		return valid;
+	}
+	
+	/*
+	 * SOLVE BUTTON LISTENER
+	 */
+
+	/**
+	 * Listener for Solve Button in TAB 1
+	 */
+	class SolveTab1Listener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {	
+			if(isMainInputValid()) {
+				System.out.println(isMainInputValid());
+				myModel.solveHyperGeomDist();
+				myView.setProbability(myModel.roundOff(myModel.getProbability()));
+			}
+			
+			System.out.println("Solve: Tab 1");
+		}	
+	}
+	
+	/**
+	 * Listener for Solve Button in TAB 2
+	 */
+	class SolveTab2Listener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {			
-			try {
-				myModel.setBigN(myView.getBigN());
-				myModel.setSmallN1(myView.getSmallN1());
-				myModel.setK1(myView.getK1());
-				myModel.setX1(myView.getX1());
-				
-				myModel.solveHyperGeomDist();
-				
-				myView.setProbability(myModel.getProbability());
-			} catch (Exception ex) {
-				ex.printStackTrace();
+			if(isMainInputValid() && isInputX2Valid()) {				
+				myModel.solveHyperGeomRangeX();
+				myView.setProbability(myModel.roundOff(myModel.getProbability()));
 			}
+			
+			System.out.println("Solve: Tab 2");
+		}	
+	}
+	
+	/**
+	 * Listener for Solve Button in TAB 3
+	 */
+	class SolveTab3Listener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {			
+			if(isMainInputValid() && isInputN2Valid()) {				
+				//myModel.solveHyperGeomRangeX();
+				myView.setProbability("N/A");
+			}
+			
+			System.out.println("Solve: Tab 3");
+		}	
+	}
+	
+	/**
+	 * Listener for Solve Button in TAB 4
+	 */
+	class SolveTab4Listener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {			
+			if(isMainInputValid() && isInputK2Valid()) {				
+				//myModel.solveHyperGeomRangeX();
+				myView.setProbability("N/A");
+			}
+			
+			System.out.println("Solve: Tab 4");
 		}	
 	}
 }
--- ST-STAT Proj2/src/Model.java
+++ ST-STAT Proj2/src/Model.java
@@ -1,76 +1,103 @@
 import jstats.Statistic;
 
-
+/**
+ * MODEL:
+ * Handles all functionalities
+ */
 public class Model {
-	private double bigN;
-	private double smallN1;
-	private double smallN2;
-	private double k1;
-	private double k2;
-	private double x1;
-	private double x2;
-	private double probability;
+	private int bigN;
+	private int smallN1;
+	private int smallN2;
+	private int k1;
+	private int k2;
+	private int x1;
+	private int x2;
+	private double probability = 0;
 	
+	/**
+	 * Rounds of value to 4 decimal places
+	 * @param value - value to be rounded off
+	 * @return value rounded off to 4 decimal places
+	 */
+	public double roundOff(double value) {
+		return (double) Math.round(value * 10000) / 10000;
+	}
+	
+	/**
+	 * Solves for the probability using Hypergeometric Distribution given N, n, k, and x
+	 */
 	public void solveHyperGeomDist() {
+		probability = 0;
 		probability = Statistic.combination(k1, x1) * Statistic.combination(bigN-k1, smallN1-x1) / Statistic.combination(bigN, smallN1);
 	}
 	
 	/**
+	 * Solves for the probability using Hypergeometric Distribution given N, n, k, and a range of values for x
+	 */
+	public void solveHyperGeomRangeX() {
+		probability = 0;
+		for(int i = x1; i <= x2; i++) {
+			probability += Statistic.combination(k1, i) * Statistic.combination(bigN-k1, smallN1-i) / Statistic.combination(bigN, smallN1);
+		}
+	}
+	
+	/*
 	 * GETTERs and SETTERs
 	 */
-	public double getBigN() {
+	
+	public int getBigN() {
 		return bigN;
 	}
 
-	public void setBigN(double bigN) {
+	public void setBigN(int bigN) {
 		this.bigN = bigN;
 	}
 
-	public double getSmallN1() {
+	public int getSmallN1() {
 		return smallN1;
 	}
 
-	public void setSmallN1(double smallN1) {
+	public void setSmallN1(int smallN1) {
 		this.smallN1 = smallN1;
 	}
 
-	public double getSmallN2() {
+	public int getSmallN2() {
 		return smallN2;
 	}
 
-	public void setSmallN2(double smallN2) {
+	public void setSmallN2(int smallN2) {
 		this.smallN2 = smallN2;
 	}
 
-	public double getK1() {
+	public int getK1() {
 		return k1;
 	}
 
-	public void setK1(double k1) {
+	public void setK1(int k1) {
 		this.k1 = k1;
 	}
 
-	public double getK2() {
+	public int getK2() {
 		return k2;
 	}
 
-	public void setK2(double k2) {
+	public void setK2(int k2) {
 		this.k2 = k2;
 	}
 
-	public double getX1() {
+	public int getX1() {
 		return x1;
 	}
 
-	public void setX1(double x1) {
+	public void setX1(int x1) {
 		this.x1 = x1;
 	}
 
-	public double getX2() {
+	public int getX2() {
 		return x2;
 	}
 
-	public void setX2(double x2) {
+	public void setX2(int x2) {
 		this.x2 = x2;
 	}
 	
